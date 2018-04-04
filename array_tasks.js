43573d9969bdf88e6b3303303a1a915f2fe9bb91
Angus Malloch <angusbass@hotmail.com>
2018-04-04 15:50:38 +0100

    Added method to concat arrays together.

---
+++ array_tasks.js
@@ -0,0 +1,38 @@
+var arrayTasks = {
+
+	concat: function (arr1, arr2) {
+		combinedArray = arr1.concat(arr2);
+		return combinedArray;
+	},
+
+	// insertAt: function (arr, itemToAdd, index) {
+
+	// },
+
+	// square: function (arr) {
+
+	// },
+
+	// sum: function (arr) {
+
+	// },
+
+	// findDuplicates: function (arr) {
+
+	// },
+
+	// removeAndClone: function (arr, valueToRemove) {
+
+	// },
+
+	// findIndexesOf: function (arr, itemToFind) {
+
+	// },
+
+	// sumOfAllEvenNumbersSquared: function (arr) {
+
+	// }
+
+}
+
+module.exports = arrayTasks
