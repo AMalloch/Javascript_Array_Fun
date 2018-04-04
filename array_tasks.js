d96d3835de1c04e4fafcbfb0ee9604de74d9393e
Angus Malloch <angusbass@hotmail.com>
2018-04-04 16:24:03 +0100

    Had issue with nodes being committed, added methods for concat, insert, square, sum.

--- array_tasks.js
+++ array_tasks.js
@@ -1,25 +1,41 @@
 var arrayTasks = {
 
 	concat: function (arr1, arr2) {
-		combinedArray = arr1.concat(arr2);
-		return combinedArray;
+		newArray = [];
+		for (element of arr1){
+			newArray.push(element);
+		}
+		for (element of arr2){
+			newArray.push(element);
+		}
+		return newArray;
 	},
 
-	// insertAt: function (arr, itemToAdd, index) {
+// should insert an item in an array at any index position
+	insertAt: function (arr, itemToAdd, index) {
+		arr.splice(index, 0, itemToAdd);
+			return arr;
+	},
 
-	// },
+	square: function (arr) {
+		const newArray = [];
+		for(let element of arr){
+			newArray.push(element * element);
+		}
+		return newArray;
+	},
 
-	// square: function (arr) {
+	sum: function (arr) {
+		total = 0
+		for(let element of arr){
+			total += element;
+		}
+		return total;
+	},
 
-	// },
+	findDuplicates: function (arr) {
 
-	// sum: function (arr) {
-
-	// },
-
-	// findDuplicates: function (arr) {
-
-	// },
+	},
 
 	// removeAndClone: function (arr, valueToRemove) {
 
